1abe403d038bdfc5191a5d7af662446e30e3ccc6
Paul Horn <knutwalker@gmail.com>
2015-09-07 13:44:37 +0200

    Queue message that were send before the websocket was ready

--- frontend/src/main/scala/de/knutwalker/wdcm/Websockets.scala
+++ frontend/src/main/scala/de/knutwalker/wdcm/Websockets.scala
@@ -4,6 +4,7 @@ import org.scalajs.dom
 import org.scalajs.dom.raw.WebSocket
 import org.scalajs.dom.{ ErrorEvent, Event, MessageEvent }
 
+import scala.collection.mutable
 import scala.scalajs.js
 import scala.scalajs.js.typedarray.TypedArrayBufferOps
 
@@ -14,6 +15,7 @@ trait Websockets {
 
   private[this] var _unbind: () ⇒ Unit             = () ⇒ ()
   private[this] var ws     : js.UndefOr[WebSocket] = js.undefined
+  private[this] val queue                          = mutable.Queue.empty[ByteBuffer]
 
   final def getWebsocketUri: String = {
     val wsProtocol = if (dom.document.location.protocol == "https:") "wss" else "ws"
@@ -35,9 +37,12 @@ trait Websockets {
     ws
   }
 
-    ws.filter(_.readyState == WebSocket.OPEN).foreach(_.send(bb.arrayBuffer()))
-
   final def send(bb: ByteBuffer) = {
+    ws.filter(_.readyState == WebSocket.OPEN).fold {
+      println(s"Got a message to send, but Websocket is not ready yet, buffering the message instead.")
+      queue.enqueue(bb)
+    }(_.send(bb.arrayBuffer()))
+  }
 
   protected final def registerUnbind(f: js.Function0[Unit]): Unit = {
     val before = _unbind
@@ -59,6 +64,12 @@ trait Websockets {
   protected final def aroundOnOpen(ws: WebSocket, event: Event): Unit = {
     this.ws = ws
     onOpen(ws, event)
+    if (queue.nonEmpty) {
+      println(s"Sending ${queue.size} messages that had arrived before websocket was open.")
+      while (queue.nonEmpty) {
+        send(queue.dequeue())
+      }
+    }
   }
 
   protected def onError(ws: WebSocket, event: ErrorEvent): Unit = ()
